56b21ee1cd571dc83937aaf925ce45874a49f1d8
tomdufall <thomas.dufall@metoffice.gov.uk>
2020-12-07 12:22:45 +0000

    Day 7 untidied

---
+++ aoc/2020/07/part2.py
@@ -0,0 +1,99 @@
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+from pathlib import Path
+import re
+from typing import Dict, List
+
+# Type alias to make type hints more obvious
+BagType = str
+
+
+@dataclass
+class BagNetwork:
+    """
+    Records what colour and quantity of bags each bag must contain.
+    Also contains a reverse link of what bags each bag can be held in.
+    """
+    must_contain: Dict[BagType, Dict[BagType, int]] = field(default_factory=dict)
+    contained_by: Dict[BagType, BagType] = field(default_factory=dict)
+
+    @staticmethod
+    def from_file(filepath: Path) -> BagNetwork:
+        bag_network = BagNetwork()
+        req_list = filepath.read_text().splitlines()
+        for req in req_list:
+            # format = <outer>s contain {quantity <inner> bag(s)}.
+            outer, inners_str = req.split(" bags contain ")
+            if inners_str == "no other bags.":
+                bag_network.add(outer, None, 0)
+            else:
+                bags_pattern = r"(\d+.+?) bag"
+                matches = re.findall(bags_pattern, inners_str)
+                for inner in matches:
+                    quantity, colour = inner.split(" ", 1)
+                    bag_network.add(outer, colour, int(quantity))
+        return bag_network
+
+    def add(self, outer: BagType, inner: BagType, quantity: int) -> None:
+        """
+        Add a requirement to the network
+        """
+        if self.must_contain.get(outer) is None:
+            self.must_contain[outer] = {}
+        self.must_contain[outer][inner] = quantity
+        if self.contained_by.get(inner) is None:
+            self.contained_by[inner] = set()
+        self.contained_by[inner].add(outer)
+
+    def remove(self, outer: BagType, inner: BagType) -> None:
+        """
+        Remove a requirement from the network
+        """
+        raise NotImplementedError
+
+    def count_can_contain_all(self, inner: BagType) -> int:
+        """
+        Return a count of how many bag types can contain the given bag
+        directly or indirectly.
+        """
+        can_contain = self.contained_by.get(inner, set()).copy()
+        to_check = self.contained_by.get(inner, set()).copy()
+        checked = {inner}
+        # print(len(can_contain), can_contain)
+        while to_check:
+            item = to_check.pop()
+            if item in checked:
+                continue
+            else:
+                can_contain.update(self.contained_by.get(item, set()))
+                to_check.update(self.contained_by.get(item, set()))
+                checked.add(item)
+        #     print(len(can_contain), can_contain)
+        # print(len(can_contain), can_contain)
+        return len(can_contain)
+
+    def must_contain_count_all(self, inner: BagType) -> int:
+        """
+        Return a count of how many bags the given bag type must contain.
+        """
+        # list of bag, quantity
+        to_add = [(inner, 1)]
+        count = 0
+        while to_add:
+            print(to_add)
+            outer, outer_quantity = to_add.pop()
+            reqs = self.must_contain.get(outer, {}).items()
+            for bag, inner_quantity in reqs:
+                if bag is not None:
+                    to_add.append((bag, outer_quantity * inner_quantity))
+            count += outer_quantity
+        return count -1 # don't counter bag itself
+
+
+if __name__ == "__main__":
+#    filepath = Path(__file__).parent / "sample_input.txt"
+    filepath = Path(__file__).parent / "input.txt"
+    network = BagNetwork.from_file(filepath)
+#    print(network.contained_by)
+    print(network.must_contain_count_all("shiny gold"))
