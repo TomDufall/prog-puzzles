b241da5a5e92344627e3f987f9b82185797bd6b6
Tom Dufall <tom@tomdufall.co.uk>
2020-12-12 02:31:14 +0000

    Tidy day 11

--- aoc/2020/11/part1.py
+++ aoc/2020/11/part1.py
@@ -1,25 +1,38 @@
 from copy import deepcopy
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from itertools import product
 from pathlib import Path
 from typing import List
 
+StringArray2D = List[List[str]]
+
 
 @dataclass
 class SeatingSimulation:
-    layout: List[List[str]]
+    layout: StringArray2D
+    current: StringArray2D = field(init=False)
+    crowding_limit: int = 3
 
     def __post_init__(self):
         self.current = self.layout
 
     @staticmethod
-    def count_surrounding_character(layout: List[List[str]], x: int, y: int, character: str) -> int:
-        to_check = [
-            (x - 1, y - 1), (x, y - 1), (x + 1, y - 1), (x - 1, y), (x + 1, y), (x - 1, y + 1), (x, y + 1), (x + 1, y + 1)
-        ]
+    def count_surrounding_character(
+            layout: StringArray2D,
+            x: int,
+            y: int,
+            character: str,
+    ) -> int:
+        to_check = product([-1, 0, 1], [-1, 0, 1])
         count = 0
-        for x2, y2 in to_check:
-            if x2 < 0 or y2 < 0 or y2 >= len(layout) or x2 >= len(layout[y2]):
+        max_x = len(layout[0]) - 1
+        max_y = len(layout) - 1
+        for dx, dy in to_check:
+            if dx == 0 and dy == 0:
+                continue
+            x2 = x + dx
+            y2 = y + dy
+            if x2 < 0 or y2 < 0 or x2 > max_x or y2 > max_y:
                 continue
             elif layout[y2][x2] == character:
                 count += 1
@@ -34,10 +47,9 @@ class SeatingSimulation:
             count = self.count_surrounding_character(current, x, y, "#")
             if current[y][x] == "L" and count == 0:
                 next_[y][x] = "#"
-            elif current[y][x] == "#" and count >= 4:
+            elif current[y][x] == "#" and count > self.crowding_limit:
                 next_[y][x] = "L"
         self.current = next_
-            
 
     def run(self, limit=1000) -> int:
         step = 0
@@ -47,10 +59,9 @@ class SeatingSimulation:
             if self.current == prev:
                 return sum("".join(row).count("#") for row in self.current)
             step += 1
-            
 
 
-def load_input(filepath: Path) -> List[List[str]]:
+def load_input(filepath: Path) -> StringArray2D:
     strings = filepath.read_text().splitlines()
     return [list(string) for string in strings]
 
--- aoc/2020/11/part2.py
+++ aoc/2020/11/part2.py
@@ -1,21 +1,29 @@
-from copy import deepcopy
 from dataclasses import dataclass
 from itertools import product
 from pathlib import Path
-from typing import List
+
+from part1 import (
+    load_input,
+    SeatingSimulation as SeatingSimulationPart1,
+    StringArray2D,
+)
 
 
 @dataclass
-class SeatingSimulation:
-    layout: List[List[str]]
-
-    def __post_init__(self):
-        self.current = self.layout
+class SeatingSimulation(SeatingSimulationPart1):
+    crowding_limit: int = 4
 
     @staticmethod
-    def count_surrounding_character(layout: List[List[str]], x: int, y: int, character: str) -> int:
+    def count_surrounding_character(
+            layout: StringArray2D,
+            x: int,
+            y: int,
+            character: str,
+    ) -> int:
         to_check = product([-1, 0, 1], [-1, 0, 1])
         count = 0
+        max_x = len(layout[0]) - 1
+        max_y = len(layout) - 1
         for dx, dy in to_check:
             if dx == 0 and dy == 0:
                 continue
@@ -24,7 +32,7 @@ class SeatingSimulation:
             while True:
                 x2 = x2 + dx
                 y2 = y2 + dy
-                if x2 < 0 or y2 < 0 or y2 >= len(layout) or x2 >= len(layout[y2]):
+                if x2 < 0 or y2 < 0 or x2 > max_x or y2 > max_y:
                     break
                 elif layout[y2][x2] == character:
                     count += 1
@@ -32,35 +40,6 @@ class SeatingSimulation:
                 elif layout[y2][x2] == "L":
                     break
         return count
-
-    def step(self) -> None:
-        current = self.current
-        next_ = deepcopy(current)
-        for x, y in product(range(len(current[0])), range(len(current))):
-            if current[y][x] == ".":
-                continue
-            count = self.count_surrounding_character(current, x, y, "#")
-            if current[y][x] == "L" and count == 0:
-                next_[y][x] = "#"
-            elif current[y][x] == "#" and count >= 5:
-                next_[y][x] = "L"
-        self.current = next_
-            
-
-    def run(self, limit=1000) -> int:
-        step = 0
-        while step < limit:
-            prev = self.current
-            self.step()
-            if self.current == prev:
-                return sum("".join(row).count("#") for row in self.current)
-            step += 1
-            
-
-
-def load_input(filepath: Path) -> List[List[str]]:
-    strings = filepath.read_text().splitlines()
-    return [list(string) for string in strings]
 
 
 if __name__ == "__main__":
