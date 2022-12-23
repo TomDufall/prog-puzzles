2a7944703e577353ac0b3dc65a6703f85aa67d87
Tom Dufall <tom@tomdufall.co.uk>
2022-12-23 02:53:27 +0000

    Unpack argv

--- 2spooky4me/s.py
+++ 2spooky4me/s.py
@@ -1,4 +1,5 @@
 from itertools import *
 import sys
 a=str.isalpha
-print(*[v if a(v:="".join(g)) else eval(v+sys.argv[2].replace("^","**").replace("/","//")) for _,g in groupby(sys.argv[1],a)],sep="")
+_,x,y=sys.argv
+print(*[v if a(v:="".join(g)) else eval(v+y.replace("^","**").replace("/","//")) for _,g in groupby(x,a)],sep="")
